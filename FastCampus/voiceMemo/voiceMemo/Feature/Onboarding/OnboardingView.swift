d6a1eaaae7a282237f4105400ae15afae1b6b302
Hsungjin <hsungjin142@gmail.com>
2023-12-24 19:38:13 +0900

    [Add] Timer 강의 내용 추가

--- FastCampus/voiceMemo/voiceMemo/Feature/Onboarding/OnboardingView.swift
+++ FastCampus/voiceMemo/voiceMemo/Feature/Onboarding/OnboardingView.swift
@@ -15,7 +15,7 @@ struct OnboardingView: View {
         // TODO: - 화면 전환 구현 필요
         NavigationStack(path: $pathModel.paths) {
 //            OnboardingContentView(onboardingViewModel: onboardingViewModel)
-            VoiceRecorderView()
+            TimerView()
                 .environmentObject(todoListViewModel)
                 .navigationDestination(for: PathType.self, destination: { pathType in
                     switch pathType {
