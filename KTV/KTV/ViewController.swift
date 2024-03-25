8504b1fa524e58daa279ecbdd816c3ea6f91a7cd
Hsungjin <hsungjin142@gmail.com>
2024-03-25 11:01:12 +0900

    KTV 초기 세팅

---
+++ KTV/KTV/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  KTV
+//
+//  Created by 황성진 on 3/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var testButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func action(_ sender: Any) {
+        print("ggg")
+    }
+    
+}
+
